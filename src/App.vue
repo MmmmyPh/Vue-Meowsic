20d568a544940c14338b2022ce8529b44b869197
mosesyzh <momente@aliyun.com>
2017-08-31 18:22:42 +0800

    first commit vue-meowsic

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <m-header></m-header>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+	import MHeader from 'components/m-header/m-header';
+
+	export default {
+		name: 'app',
+		components: {
+			MHeader
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	@import "~common/stylus/variable"
+
+	#app
+		color: $color-theme
+		font-size: $font-size-nm
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id='m-header' class="m-header">
+		<div class="m-header__logo"></div>
+		<div class="m-header__text">Meowsic</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'm-header'
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import "~common/stylus/variable"
+	@import "~common/stylus/mixin";
+
+	.m-header
+		display: flex
+		flex-flow: row nowrap
+		justify-content: center
+		align-items: center
+		position: absolute
+		top: 0
+		right: 0
+		left: 0
+		height: 44px
+		color: $color-theme
+		font-size: $font-size-theme
+		text-align: center
+		&__logo
+			margin-right: 10px
+			width: 32px
+			height: 32px
+			bg-image("logo")
+			background-size: contain
+			background-position 0 -2px
+		&__text
+			line-height: 32px
+
+</style>
